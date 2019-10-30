3d3e308a84a6fdeee47e3127e744f964e75d15d0
Priya2597 <57185541+Priya2597@users.noreply.github.com>
2019-10-30 22:58:47 +0530

    Update code.c

--- code.c
+++ code.c
@@ -2,14 +2,14 @@
 
 int main()
 {
- int a[50],n,j,i,temp;
+ int a[0],n,j,i,temp;
  printf("Enter the size of an array");
  scanf("%d",&n);
  printf("Enter the array elements:");
  for (i=0;i<n;i++)
    scanf("%d",&a[i]);
    
-   for(i=1;i<=n;i++)
+   for(i=1;i<n;i++)
       for(j=0;j<(n-1);j++)
         if(a[j]>a[j+1])
          {
@@ -19,7 +19,7 @@ int main()
          }
          printf("\array after sorting:");
          for(i=0;i<n;i++)
-         printf("%d",a[i]);
+         printf("%u",a[i]);
          
         return 0;
         
